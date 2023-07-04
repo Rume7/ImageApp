24b986332cdab6a7af5443420d18a76ac7b96002
Rume7 <electengrhume@gmail.com>
2023-07-04 20:09:32 +0100

    Added image controller.

--- src/main/java/com/example/controller/ImageController.java
+++ src/main/java/com/example/controller/ImageController.java
@@ -1,2 +1,9 @@
-package com.example.controller;public class ImageController {
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ImageController {
+
+    
 }
