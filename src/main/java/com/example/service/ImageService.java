6fae3efc51e3b2566b57ae89814e169a67e84f19
Rume7 <electengrhume@gmail.com>
2023-07-04 20:48:09 +0100

    Improved service class.

--- src/main/java/com/example/service/ImageService.java
+++ src/main/java/com/example/service/ImageService.java
@@ -1,14 +1,15 @@
 package com.example.service;
 
+import com.example.dto.ImageDTO;
 import com.example.entities.Image;
 
 import java.util.List;
 
 public interface ImageService {
 
-    Image createImage(Image image);
+    Image createImage(ImageDTO image);
 
-    Image updateImage(Image image);
+    Image updateImage(Image image, Long id);
 
     List<Image> getAllImages() ;
 
--- src/main/java/com/example/service/ImageServiceImpl.java
+++ src/main/java/com/example/service/ImageServiceImpl.java
@@ -1,13 +1,17 @@
 package com.example.service;
 
+import com.example.dto.ImageDTO;
 import com.example.entities.Image;
+import com.example.entities.Tag;
 import com.example.repositories.ImageRepository;
+import com.example.repositories.TagRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -16,14 +20,26 @@ public class ImageServiceImpl implements ImageService {
     @Autowired
     private final ImageRepository imageRepository;
 
+    @Autowired
+    private final TagRepository tagRepository;
+
     @Override
-    public Image createImage(Image image) {
-        Image img = imageRepository.save(image);
+    public Image createImage(ImageDTO image) {
+        List<Tag> allTags = mapTagsToList(image.tags());
+        Image img = new Image(image.name(), allTags);
+        img.setTags(allTags);
+        //tagRepository.save(allTags);
+        Image imgGotten = imageRepository.save(img);
         return img;
     }
 
+    private List<Tag> mapTagsToList(Set<Tag> tags) {
+
+        return null;
+    }
+
     @Override
-    public Image updateImage(Image image) {
+    public Image updateImage(Image imageDTO, Long id) {
         return null;
     }
 
