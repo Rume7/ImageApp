eeb4c6498ffdef7b8af938aa51a6710102ca0bbe
Rume7 <electengrhume@gmail.com>
2023-07-04 20:47:46 +0100

    Added the Tag repo

--- src/main/java/com/example/repositories/TagRepository.java
+++ src/main/java/com/example/repositories/TagRepository.java
@@ -1,2 +1,10 @@
-package com.example.repositories;public interface TagRepository {
+package com.example.repositories;
+
+import com.example.entities.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Long> {
+
 }
