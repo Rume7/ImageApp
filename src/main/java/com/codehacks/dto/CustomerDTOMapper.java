08b9e00cf0e7a2ae2ad4a9b199e44a605a5d4713
Rume7 <electengrhume@gmail.com>
2023-07-09 14:42:06 +0100

    Created Customer to CustomerDTO mapper.

---
+++ src/main/java/com/codehacks/dto/CustomerDTOMapper.java
@@ -0,0 +1,17 @@
+package com.codehacks.dto;
+
+import com.codehacks.entities.Customer;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
+
+@Service
+public class CustomerDTOMapper implements Function<Customer, CustomerDTO> {
+
+    @Override
+    public CustomerDTO apply(Customer customer) {
+        return new CustomerDTO(
+                customer.getName(), customer.getMedia()
+        );
+    }
+}
