f8ae4c5e8aae29a78b42f0f93b6438fcba26966e
Rume7 <electengrhume@gmail.com>
2023-07-09 17:45:37 +0100

    Added the Optional class to handle null case.

--- src/main/java/com/codehacks/repositories/CustomerRepository.java
+++ src/main/java/com/codehacks/repositories/CustomerRepository.java
@@ -4,10 +4,11 @@ import com.codehacks.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
 
-    Customer getCustomerById(UUID id);
+    Optional<Customer> getCustomerById(UUID id);
 }
