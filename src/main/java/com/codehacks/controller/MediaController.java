ea2f5fcbe0827f5dcaeb5100cbed3670563d410d
Rume7 <electengrhume@gmail.com>
2023-07-09 17:49:00 +0100

    Cleaned up whitespaces and added IOException.

--- src/main/java/com/codehacks/controller/MediaController.java
+++ src/main/java/com/codehacks/controller/MediaController.java
@@ -1,25 +1,26 @@
 package com.codehacks.controller;
 
 import com.codehacks.entities.Media;
-import com.codehacks.service.ImageService2;
+import com.codehacks.service.MediaService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.UUID;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("api/v1/images")
-public class ImageController2 {
+public class MediaController {
 
-    private final ImageService2 imageService;
+    private final MediaService imageService;
 
     @PostMapping("/{customerId}/upload")
     public ResponseEntity<Media> uploadImage(@PathVariable("customerId") UUID customerId,
-                                             @RequestParam("image") MultipartFile image) {
+            @RequestParam("image") MultipartFile image) throws IOException {
         Media media = imageService.uploadImageForCustomer(customerId, image);
         return new ResponseEntity<>(media, HttpStatus.CREATED);
     }
