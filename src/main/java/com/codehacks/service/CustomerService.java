88f06754e71ee91c41e5c6786a33024a8d1d9c43
Rume7 <electengrhume@gmail.com>
2023-07-10 19:13:53 +0100

    Removed the media object.

--- src/main/java/com/codehacks/service/CustomerService.java
+++ src/main/java/com/codehacks/service/CustomerService.java
@@ -2,17 +2,14 @@ package com.codehacks.service;
 
 import com.codehacks.dto.CustomerDTO;
 import com.codehacks.dto.CustomerDTOMapper;
-import com.codehacks.entities.Customer;
-import com.codehacks.entities.Media;
+import com.codehacks.entities.Customer;import com.codehacks.entities.Media;
 import com.codehacks.exception.ResourceNotFoundException;
 import com.codehacks.repositories.CustomerRepository;
 import com.codehacks.repositories.MediaRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -25,24 +22,8 @@ public class CustomerService implements ICustomerService {
     @Override
     public Customer createCustomer(CustomerDTO customerDTO) {
         Customer customer = new Customer();
-        customer.setId(UUID.randomUUID());
         customer.setName(customerDTO.name());
-        customer.setMedia(customerDTO.mediaSet());
-
-        if (customerDTO.mediaSet() != null) {
-            for (Media media : customerDTO.mediaSet()) {
-                Media newMedia = new Media();
-                newMedia.setType(media.getType());
-                newMedia.setDate(LocalDateTime.now());
-                newMedia.setImage(media.getImage());
-                newMedia.setTags(media.getTags());
-                newMedia.setCustomer(media.getCustomer());
-
-                mediaRepository.save(newMedia);
-            }
-        }
-        customerRepository.save(customer);
-        return customer;
+        return customerRepository.save(customer);
     }
 
     @Override
@@ -51,23 +32,20 @@ public class CustomerService implements ICustomerService {
     }
 
     @Override
-    public Customer getCustomerById(UUID customerId) {
+    public Customer getCustomerById(Integer customerId) {
         return findOrThrow(customerId);
     }
 
     @Override
-    public Customer updateCustomer(UUID customerId, CustomerDTO cust) {
+    public Customer updateCustomer(Integer customerId, CustomerDTO cust) {
         var customer = findOrThrow(customerId);
         if (cust.name() != null) {
             customer.setName(cust.name());
         }
-        if (cust.mediaSet() != null) {
-            customer.setMedia(cust.mediaSet());
-        }
         return customerRepository.save(customer);
     }
 
-    private Customer findOrThrow(final UUID id) {
+    private Customer findOrThrow(final Integer id) {
         return customerRepository.findById(id)
                 .orElseThrow(ResourceNotFoundException::new);
     }
