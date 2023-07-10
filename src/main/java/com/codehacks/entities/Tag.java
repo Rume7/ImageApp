06f7d4c2bbac55160cb45f839067f091acec6f41
Rume7 <electengrhume@gmail.com>
2023-07-10 19:06:11 +0100

    Created a person object to represent tagged entities

--- src/main/java/com/codehacks/entities/Tag.java
+++ src/main/java/com/codehacks/entities/Tag.java
@@ -2,14 +2,18 @@ package com.codehacks.entities;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
 @JsonSerialize
 public class Tag implements Serializable {
 
@@ -24,4 +28,25 @@ public class Tag implements Serializable {
     @ManyToOne
     @JoinColumn(name = "media_id")
     private Media media;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMedia(Media media) {
+        this.media = media;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tag tag = (Tag) o;
+        return tagId.equals(tag.tagId) && name.equals(tag.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tagId, name);
+    }
 }
