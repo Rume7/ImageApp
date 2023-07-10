b04c4fe9c6de556109b66233ecd31bfc7f2eb462
Rume7 <electengrhume@gmail.com>
2023-07-10 19:08:47 +0100

    Added method to tag an image.

--- src/main/java/com/codehacks/service/MediaService.java
+++ src/main/java/com/codehacks/service/MediaService.java
@@ -1,14 +1,18 @@
 package com.codehacks.service;
 
 import com.codehacks.entities.Media;
+import com.codehacks.entities.Person;
+import com.codehacks.entities.Tag;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.List;
+import java.util.Set;
 
 public interface MediaService {
 
-    Media uploadImageForCustomer(UUID customerId, MultipartFile image)
+    Media uploadImageForCustomer(Integer customerId, MultipartFile image)
             throws IOException;
+
+    Set<Tag> tagAnImage(List<Person> names, Integer media);
 }
--- src/main/java/com/codehacks/service/MediaServiceImpl.java
+++ src/main/java/com/codehacks/service/MediaServiceImpl.java
@@ -2,9 +2,12 @@ package com.codehacks.service;
 
 import com.codehacks.entities.Customer;
 import com.codehacks.entities.Media;
+import com.codehacks.entities.Person;
+import com.codehacks.entities.Tag;
 import com.codehacks.exception.ResourceNotFoundException;
 import com.codehacks.repositories.CustomerRepository;
 import com.codehacks.repositories.MediaRepository;
+import com.codehacks.repositories.TagRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,20 +15,23 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class MediaServiceImpl implements MediaService {
 
     @Autowired
-    private final MediaRepository mediaRepository;
-
-    @Autowired
     private final CustomerRepository customerRepository;
+    @Autowired
+    private final MediaRepository mediaRepository;
+    @Autowired
+    private final TagRepository tagRepository;
 
-    public Media uploadImageForCustomer(UUID customerId, MultipartFile image) throws IOException {
+    public Media uploadImageForCustomer(Integer customerId, MultipartFile image) throws IOException {
         Optional<Customer> customer = customerRepository.findById(customerId);
         if (customer.isPresent()) {
             return uploadImage(image, customer.get());
@@ -35,17 +41,26 @@ public class MediaServiceImpl implements MediaService {
 
     private Media uploadImage(MultipartFile image, Customer customer) throws IOException {
         Media media = new Media();
-        media.setId(generateKey());
         media.setType(image.getContentType());
         media.setDate(LocalDateTime.now());
         media.setImage(image.getBytes());
         media.setCustomer(customer);
-
-        mediaRepository.save(media);
-        return media;
+        return mediaRepository.saveAndFlush(media);
     }
 
-    private UUID generateKey() {
-        return UUID.randomUUID();
+    public Set<Tag> tagAnImage(List<Person> names, Integer mediaId) {
+        Optional<Media> media = mediaRepository.findById(mediaId);
+        if (media.isPresent()) {
+            Set<Tag> allTags = new HashSet<>();
+            for (Person person : names) {
+                Tag aTag = new Tag();
+                aTag.setName(person.toString());
+                aTag.setMedia(media.get());
+                allTags.add(aTag);
+                tagRepository.saveAndFlush(aTag);
+            }
+            return allTags;
+        }
+        throw new ResourceNotFoundException();
     }
 }
