2fa5b757b893d6bacdea29d5439bf20321372e57
Rume7 <electengrhume@gmail.com>
2023-07-10 19:06:50 +0100

    Changed all class ids to data type Integer.

--- src/main/java/com/codehacks/repositories/CustomerRepository.java
+++ src/main/java/com/codehacks/repositories/CustomerRepository.java
@@ -5,10 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-import java.util.UUID;
 
 @Repository
-public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
-    Optional<Customer> getCustomerById(UUID id);
+    Optional<Customer> getCustomerById(Integer id);
 }
--- src/main/java/com/codehacks/repositories/MediaRepository.java
+++ src/main/java/com/codehacks/repositories/MediaRepository.java
@@ -4,9 +4,7 @@ import com.codehacks.entities.Media;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
-public interface MediaRepository extends JpaRepository<Media, UUID> {
+public interface MediaRepository extends JpaRepository<Media, Integer> {
 
 }
