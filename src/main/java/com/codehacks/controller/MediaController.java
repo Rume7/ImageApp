a77c68837291ffa3c26cc3d2ba7277e974f45f20
Rume7 <electengrhume@gmail.com>
2023-07-10 19:14:22 +0100

    Created a method to handle tagging of images.

--- src/main/java/com/codehacks/controller/MediaController.java
+++ src/main/java/com/codehacks/controller/MediaController.java
@@ -1,6 +1,8 @@
 package com.codehacks.controller;
 
 import com.codehacks.entities.Media;
+import com.codehacks.entities.Person;
+import com.codehacks.entities.Tag;
 import com.codehacks.service.MediaService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -9,19 +11,27 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.UUID;
+import java.util.List;
+import java.util.Set;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("api/v1/images")
+@RequestMapping("api/v1/media")
 public class MediaController {
 
     private final MediaService mediaService;
 
-    @PostMapping("/{customerId}/upload")
-    public ResponseEntity<Media> uploadImage(@PathVariable("customerId") UUID customerId,
+    @PostMapping("/{mediaId}/upload")
+    public ResponseEntity<Media> uploadImage(@PathVariable("mediaId") Integer mediaId,
             @RequestParam("image") MultipartFile image) throws IOException {
-        Media media = mediaService.uploadImageForCustomer(customerId, image);
+        Media media = mediaService.uploadImageForCustomer(mediaId, image);
         return new ResponseEntity<>(media, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/{mediaId}/tag")
+    public ResponseEntity<Set<Tag>> tagAnImage(@RequestBody List<Person> names,
+                                          @PathVariable("mediaId") Integer mediaId) {
+        Set<Tag> tags = mediaService.tagAnImage(names, mediaId);
+        return new ResponseEntity<>(tags, HttpStatus.CREATED);
     }
 }
